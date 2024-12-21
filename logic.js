fd679cc1ca9198d6d2cd7f547e96c7f0dd4d97c8
GarDeN-bear <96305662+GarDeN-bear@users.noreply.github.com>
2024-12-21 16:07:18 +0300

    Update logic.js

--- logic.js
+++ logic.js
@@ -17,45 +17,55 @@ function startGame() {
   renderBoard(board);
 }
 
-function isWin(board) {
-  for (let row = 0; row < board.length; ++row) {
-    let countByRow = 0;
-    let countByColumn = 0;
-    let countByDiagonal = 0;
-    for (let column = 0; column < board.length; ++column) {
-      if (board[row][column] == players[activePlayer]) {
-        ++countByColumn;
-      }
-      if (board[column][row] == players[activePlayer]) {
-        ++countByRow;
-      }
-      if (board[column][column] == players[activePlayer]) {
-        ++countByDiagonal;
-      } else if (
-        board[column][board.length - column - 1] == players[activePlayer]
-      ) {
-        ++countByDiagonal;
-      }
-      if (
-        countByColumn == board.length ||
-        countByRow == board.length ||
-        countByDiagonal == board.length
-      ) {
-        return true;
+function isWin(row, column, board) {
+  function checkRow() {
+    for (let i = 0; i < board.length; ++i) {
+      if (board[row][i] != players[activePlayer]) {
+        return false;
       }
     }
+    return true;
   }
-  return false;
+
+  function checkColumn() {
+    for (let i = 0; i < board.length; ++i) {
+      if (board[i][column] != players[activePlayer]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function checkDiagonal() {
+    let isFirstDiagonalWin = true;
+    let isSecondDiagonalWin = true;
+    for (let i = 0; i < board.length; ++i) {
+      if (board[i][i] != players[activePlayer]) {
+        isFirstDiagonalWin = false;
+        break;
+      }
+    }
+    for (let i = 0; i < board.length; ++i) {
+      if (board[i][board.length - i - 1] != players[activePlayer]) {
+        isSecondDiagonalWin = false;
+        break;
+      }
+    }
+    return isFirstDiagonalWin || isSecondDiagonalWin;
+  }
+
+  return checkRow() || checkColumn() || checkDiagonal();
 }
 
 function click(row, column) {
   board[row][column] = players[activePlayer];
   renderBoard(board);
-  if (isWin(board)) {
+  if (isWin(row, column, board)) {
     showWinner(activePlayer);
   } else {
-    activePlayer == 0 ? (activePlayer = 1) : (activePlayer = 0);
+    activePlayer = activePlayer == 0 ? 1 : 0;
   }
 }
 
 startGame();
+
